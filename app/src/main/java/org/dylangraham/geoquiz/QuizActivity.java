9eea6a9d6fb93ec25a168955f6a908a50d8fb2c6
Dylan Graham <dylan.graham@gmail.com>
2016-03-10 15:47:21 +1100

    Add questions

--- app/src/main/java/org/dylangraham/geoquiz/QuizActivity.java
+++ app/src/main/java/org/dylangraham/geoquiz/QuizActivity.java
@@ -4,20 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+    private int currentIndex = 0;
+
+    private Question[] questionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, true),
+            new Question(R.string.question_africa, true),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        questionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        nextButton = (Button) findViewById(R.id.next_button);
         trueButton = (Button) findViewById(R.id.true_button);
         falseButton = (Button) findViewById(R.id.false_button);
+
+        nextQuestion();
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex + 1) % questionBank.length;
+                nextQuestion();
+            }
+        });
 
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +57,10 @@ public class QuizActivity extends AppCompatActivity {
                 Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void nextQuestion() {
+        int question = questionBank[currentIndex].getTextResID();
+        questionTextView.setText(question);
     }
 }
