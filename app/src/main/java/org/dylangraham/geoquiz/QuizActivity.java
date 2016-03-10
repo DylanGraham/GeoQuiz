b2d1310fac26ecfbd1fb8c1b02c074f733f934ad
Dylan Graham <dylan.graham@gmail.com>
2016-03-10 22:09:05 +1100

    Swap ImageButtons to Buttons

--- app/src/main/java/org/dylangraham/geoquiz/QuizActivity.java
+++ app/src/main/java/org/dylangraham/geoquiz/QuizActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +13,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
-    private ImageButton nextButton;
-    private ImageButton prevButton;
+    private Button nextButton;
+    private Button prevButton;
     private TextView questionTextView;
     private int currentIndex = 0;
 
@@ -34,8 +33,8 @@ public class QuizActivity extends AppCompatActivity {
 
         questionTextView = (TextView) findViewById(R.id.question_text_view);
 
-        nextButton = (ImageButton) findViewById(R.id.next_button);
-        prevButton = (ImageButton) findViewById(R.id.prev_button);
+        nextButton = (Button) findViewById(R.id.next_button);
+        prevButton = (Button) findViewById(R.id.prev_button);
         trueButton = (Button) findViewById(R.id.true_button);
         falseButton = (Button) findViewById(R.id.false_button);
 
